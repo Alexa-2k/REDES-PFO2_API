1088363fe605fcafd2f519c203d866dd24acaeb4
Alexa-2k <subscrip.lr@gmail.com>
2025-09-29 10:38:37 -0300

    Update Readme.md

--- init_db.py
+++ init_db.py
@@ -1,24 +1,8 @@
-import sqlite3
-
-conn = sqlite3.connect("tareas.db")
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    usuario TEXT UNIQUE NOT NULL,
-    contraseña TEXT NOT NULL
-)
-""")
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS tareas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    usuario TEXT NOT NULL,
-    descripcion TEXT NOT NULL
-)
-""")
-
-conn.commit()
-conn.close()
+import sqlite3 
+conn = sqlite3.connect("tareas.db") 
+cursor = conn.cursor() 
+cursor.execute(""" CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT UNIQUE NOT NULL, contraseña TEXT NOT NULL ) """) 
+cursor.execute(""" CREATE TABLE IF NOT EXISTS tareas ( id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT NOT NULL, descripcion TEXT NOT NULL ) """) 
+conn.commit() 
+conn.close() 
 print("Base de datos creada: tareas.db")
